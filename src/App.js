e088154d1c71d4e236c381fb53a9b03fbdb2b2de
Dmytro Anikin <dimianni@users.noreply.github.com>
2022-04-07 14:13:46 -0500

    test

--- src/App.js
+++ src/App.js
@@ -19,8 +19,8 @@ class App extends Component {
           <main>
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="about" element={<About />} />
-              <Route path="shopnow" element={<ShopNow />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/shopnow" element={<ShopNow />} />
               <Route path="/product/:id" element={<Product />} />
 
             </Routes>
